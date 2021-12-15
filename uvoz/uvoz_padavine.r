19b9594fb13c4dcb2aeec6aefb786e4bc4a56ddc
Maj Cufer <maj.cufer@gmail.com>
2021-12-15 13:06:38 +0100

    leto in mesec spremenjena v integer

---
+++ uvoz/uvoz_padavine.r
@@ -0,0 +1,31 @@
+# 2. faza: Uvoz podatkov
+library(readr)
+
+seznam.map <- list.files(path="podatki/Padavine", full.names = TRUE)
+
+for (mapa in seznam.map) {
+  
+  seznam.datotek <- list.files(path=mapa, pattern = "^RR", full.names = TRUE)
+  podatki <- read_csv(seznam.datotek, skip = 19) %>% select(DATE, RR)
+  podatki$RR[podatki$RR == -9999] <- NA
+  
+  stevilo.postaj <- sum(podatki$DATE == 20010501)
+  
+  ################################################
+  
+  racunanje <- podatki %>% filter(DATE > 20151231) %>% group_by(DATE) %>% 
+    summarise(padavine.dnevno=sum(RR, na.rm=T),
+              stevilo.na=sum(is.na(RR)))
+  racunanje$povprecno <- racunanje$padavine.dnevno / (stevilo.postaj - racunanje$stevilo.na)
+  
+  
+  ################ KONČNA TABELA #################
+  
+  tabela <- tibble(leto=substr(racunanje$DATE, 1, 4) %>% as.integer(),
+                   mesec=substr(racunanje$DATE, 5, 6) %>% as.integer(),
+                   dan=substr(racunanje$DATE, 7, 8) %>% as.integer(),
+                   padavine=racunanje$povprecno) %>% group_by(leto, mesec) %>% 
+    summarise(padavine=round(sum(padavine)/10))
+  
+  assign(basename(mapa), tabela)
+}
